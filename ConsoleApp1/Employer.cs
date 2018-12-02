7610247ae85d71f983af9b28e3a6208cc2a59147
Omarov Islam Ilham  <Omar_zi02@ITSTEP.LOCAL>
2018-12-02 13:00:22 +0400

    Add project files.

---
+++ ConsoleApp1/Employer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Employer : User
+    {
+        public Employer(string usename, string email, string password) : base(usename, email, password){}
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public static bool IsEmail(string mail)
+    {
+        string MailPattern = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+        return Regex.IsMatch(mail, MailPattern);
+    }
+
+    public static bool IsPassword(string password)
+    {
+        string PasswordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$";
+        return Regex.IsMatch(password, PasswordPattern);
+    }
+
+    public static bool IsNumber(string num)
+    {
+        string pNumberPattern = @"^[50|51|55|70|77]{2}[0-9]{7}$";
+        return Regex.IsMatch(num, pNumberPattern);
+    }
+}
